bd0cb13e8e43eecab777b51601b468703d96e52c
1 <a1@1deMacBook-Pro.local>
2019-06-19 19:51:51 +0800

    add kernelValue and sys.path

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 from mathutils import Vector
 def point_at(obj, direction):
     if(type(direction)!=Vector):
@@ -8,8 +9,18 @@ def point_at(obj, direction):
 
     # assume we're using euler rotation
     obj.rotation_euler = rot_quat.to_euler()
+    return
+def GaussianKernelValue(point1,point2):
+    distance=(point1[0]-point2[0])**2+(point1[1]-point2[1])**2+(
+        point1[2]-point2[2])**2
+    distance=distance ** 0.5
+    print('distance is: {}'.format(distance))
+    return
+print('imported!')
+if __name__ == 'main':
+    arrow = bpy.data.objects['Arrow']
+    point_at(arrow,(1,1,0))
 
-arrow = bpy.data.objects['Arrow']
-point_at(arrow,(0,1,0))
+
 
 
