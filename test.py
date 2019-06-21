34696e25bd8065c7f2823f217ebc5c2f7f0b1483
1 <a1@1deMacBook-Pro.local>
2019-06-21 17:29:32 +0800

    message

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-import bpy
+#import bpy
 import math
-from mathutils import Vector
+#from mathutils import Vector
+import numpy as np
 def point_at(obj, direction):
     if(type(direction)!=Vector):
        direction=Vector(direction)
@@ -17,9 +18,24 @@ def GaussianKernelValue(point1,point2):
     print('distance is: {}'.format(distance))
     return
 print('imported!')
-if __name__ == 'main':
-    arrow = bpy.data.objects['Arrow']
-    point_at(arrow,(1,1,0))
+if __name__ == '__main__':
+    #arrow = bpy.data.objects['Arrow']
+    #point_at(arrow,(1,1,0))
+    testMat=np.array([[1,2,2],[2,1,2],[2,2,1]])
+    evals,evecs=np.linalg.eig(testMat)
+    result=np.zeros((3,3))
+    print(evals)
+    big2small_indices = np.argsort(evals).tolist()
+    big2small_indices.reverse()
+
+    for i in range(3):
+        eval=evals[big2small_indices[i]]
+        print('eval is: {}'.format(eval))
+        result+=eval*np.dot(evecs[:,[big2small_indices[i]]],
+        np.reshape(evecs[:,big2small_indices[i]],(1,3)))
+        #print(result)
+        pass
+    
 
 
 
